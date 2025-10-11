3747c5b84ff8e97ff1b7eb785cd1f738f780d3e9
igor-mrs <igorbstm321@gmail.com>
2025-10-11 16:49:31 -0300

    feat: finaliza sistema de drop de XP
    - XP orb spawna ao matar inimigo
    - Player coleta XP automaticamente
    - UI de experiência atualiza corretamente
    - Enemy atravessa player sem colidir
    - Remove prints de debug

--- ExperienceSystem/experience_ui.gd
+++ ExperienceSystem/experience_ui.gd
@@ -7,28 +7,18 @@ extends Control
 @onready var exp_num: Label = $ExperienceBar/ExperienceNumber
 
 func _ready() -> void:
-	print("ExperienceUI: _ready() called")
-	print("ExperienceUI: component = ", component)
-	print("ExperienceUI: component path = ", component.get_path() if component else "NULL")
-	print("ExperienceUI: bar = ", bar)
 	bar.value = component.current_experience
 	bar.max_value = component.experience_to_level
 	lvl_num.text = str(component.level)
 	_on_experience_gained(0)
-	print("ExperienceUI: Connecting to component.leveled")
 	component.leveled.connect(_on_leveled)
-	print("ExperienceUI: Connecting to component.experience_gained")
 	component.experience_gained.connect(_on_experience_gained)
-	print("ExperienceUI: Signals connected!")
 	bar.mouse_entered.connect(_on_bar_mouse_enter)
 	bar.mouse_exited.connect(_on_bar_mouse_exit)
 
 func _on_experience_gained(new_value: float) -> void:
-	print("ExperienceUI: _on_experience_gained called with ", new_value)
-	print("ExperienceUI: component.current_experience = ", component.current_experience)
 	bar.value = new_value
 	exp_num.text = "%.2f/%.2f" % [component.current_experience, component.experience_to_level]
-	print("ExperienceUI: bar.value set to ", bar.value)
 	
 func _on_leveled(new_level: int) -> void:
 	bar.max_value = component.experience_to_level
